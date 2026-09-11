712ebedc39ebb1a8d530e167ca899dfde805543f
David-G-Cotom <brandoncotom202231460@cunoc.edu.gt>
2026-09-11 16:37:22 -0600

    Inicializacion del Backend con Spring Initializr. Configuracion hecha para la conexion con la DB en PostgreSQL.

---
+++ Backend/proyecto1_archivos_ss26/src/main/java/com/mycompany/proyecto1_archivos_ss26/Proyecto1ArchivosSs26Application.java
@@ -0,0 +1,13 @@
+package com.mycompany.proyecto1_archivos_ss26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto1ArchivosSs26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto1ArchivosSs26Application.class, args);
+	}
+
+}
---
+++ Backend/proyecto1_archivos_ss26/src/test/java/com/mycompany/proyecto1_archivos_ss26/Proyecto1ArchivosSs26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.proyecto1_archivos_ss26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto1ArchivosSs26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
